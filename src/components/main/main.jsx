3496c081f95afd12f59388cada891148a8571a32
DostovS <sohibjondostov04@gmail.com>
2022-12-22 22:13:08 +0500

    add categories

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,5 +1,20 @@
-export default function Main() {
-  return (
-    <div>main</div>
-  )
+import { colors } from '../../constants/colors'
+import { Box, Container, Stack, Typography } from '@mui/material'
+import { Category } from '../'
+
+const Main = () => {
+	return (
+		<Stack>
+			<Category/>
+			<Box p={2} sx={{ height: '90vh' }}>
+				<Container maxWidth={'90%'}>
+					<Typography variant={'h4'} fontWeight={'bold'} mb={2}>
+						<span style={{ color: colors.secondary }}>videos</span>
+					</Typography>
+				</Container>
+			</Box>
+		</Stack>
+	)
 }
+
+export default Main
